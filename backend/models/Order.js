65b49e7f64982d0e5a61a36be2edf1fe3b50d86f
keyrank78-devlopers <keyrank78@gmail.com>
2026-09-07 09:53:03 +0530

    cancel order api bug fix

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -89,7 +89,7 @@ const orderSchema = new mongoose.Schema(
       },
       status: {
         type: String,
-        enum: ["Pending", "Paid", "Failed"],
+        enum: ["Pending", "Paid", "Failed", "Refunded"],
         default: "Pending",
       },
       transactionId: {
